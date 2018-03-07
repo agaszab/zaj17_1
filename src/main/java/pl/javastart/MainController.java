02ab4e774d4022e3b9f9a43ac14d5cc43fa41272
agatito <agatito@yopmail.com>
2018-03-07 21:09:18 +0100

    przerobione z request na @RequestParam

--- src/main/java/pl/javastart/MainController.java
+++ src/main/java/pl/javastart/MainController.java
@@ -3,6 +3,7 @@ package pl.javastart;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
@@ -38,18 +39,17 @@ public class MainController {
 
     @RequestMapping("/add")
     @ResponseBody
-    public String addUser(HttpServletRequest request){
-
-        String imie=request.getParameter("imie");
-        String nazwisko=request.getParameter("nazwisko");
-        int wiek=Integer.parseInt(request.getParameter("wiek"));
+    public String addUser(@RequestParam String imie, @RequestParam String nazwisko, @RequestParam String wiek){
+//        String imie=request.getParameter("imie");
+ //       String nazwisko=request.getParameter("nazwisko");
+        int age=Integer.parseInt(wiek);
 
         if (imie==null || imie.equals(""))
         {return "/err.html";}  // wejdzie strona jak puste imie
 
 
 
-     User user = new  User (imie, nazwisko, wiek);
+     User user = new  User (imie, nazwisko, age);
 
         usersList.add(user);
 
